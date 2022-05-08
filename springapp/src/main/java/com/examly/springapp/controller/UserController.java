8487b70a3b877f86f7fdb1f53b39e6038a907d98
saha-bhaswati <saha.bhaswati2000@gmail.com>
2022-05-08 08:14:42 +0000

    Modified UserController

--- springapp/src/main/java/com/examly/springapp/controller/UserController.java
+++ springapp/src/main/java/com/examly/springapp/controller/UserController.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 
 import com.examly.springapp.entity.User;
 import com.examly.springapp.service.UserService;
-
+import com.examly.springapp.exception.CustomException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
